c4e92fa37abc3b652105be272ff5133af2bfb058
Al-Edrisy <salehfree33@gmail.com>
2025-07-21 02:19:49 +0300

    Add web-specific Vite config and update GitHub Actions for web/PWA deployment

---
+++ vite.config.web.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: "::",
+    port: 5173,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      }
+    },
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+}); 
